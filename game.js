d409e8e313ae2bd7a7c7959528cc3c8ba715e9ae
kw1c-KarimovaNargiz <giesje002@gmail.com>
2025-03-16 22:32:03 +0100

    kantelen tank

--- game.js
+++ game.js
@@ -17,17 +17,18 @@ gunImg.src = "assets/Gun_01.png";
 const grenadeImg = new Image();
 grenadeImg.src = "assets/Granade_Shell.png";
 
-// Tank instellingen
+//tank
 const tank = {
     x: canvas.width / 2 - 60,
     y: canvas.height - 120,
     width: 120,
     height: 80,
     speed: 6,
-    direction: 0
+    direction: 0,
+    tiltAngle: 0
 };
 
-// Tracks
+//tracks
 const tracks = {
     width: 28,
     height: tank.height * 1.15,
@@ -35,7 +36,7 @@ const tracks = {
     offsetY: -5    
 };
 
-// Kanon
+//kanon
 const upperbody = {
     angle: 0,
     width: 75,  
@@ -44,10 +45,10 @@ const upperbody = {
     rotationSpeed: 5
 };
 
-// Granaten (vijanden)
+//granaten
 const grenades = [];
 const grenadeSpeed = 3;
-const spawnInterval = 1500; // Om de 1,5 sec spawnt een granaat
+const spawnInterval = 1500; 
 
 function spawnGrenade() {
     grenades.push({
@@ -59,12 +60,14 @@ function spawnGrenade() {
     });
 }
 
-// Toetsen voor tank en kanonbesturing
+//key
 document.addEventListener("keydown", (event) => {
     if (event.key === "a" || event.key === "A") {
         tank.direction = -1;
+        tank.tiltAngle = -10; 
     } else if (event.key === "d" || event.key === "D") {
         tank.direction = 1;
+        tank.tiltAngle = 10; 
     } else if (event.key === "ArrowLeft") {
         upperbody.angle -= upperbody.rotationSpeed;
         if (upperbody.angle < -45) upperbody.angle = -45;
@@ -78,21 +81,20 @@ document.addEventListener("keyup", (event) => {
     if ((event.key === "a" || event.key === "A" || event.key === "d" || event.key === "D") && 
         tank.direction === (event.key.toLowerCase() === "a" ? -1 : 1)) {
         tank.direction = 0;
+        tank.tiltAngle = 0; 
     }
 });
 
-// **Update functie**
+
 function update() {
     tank.x += tank.speed * tank.direction;
     
     if (tank.x < 50) tank.x = 50;
     if (tank.x + tank.width > canvas.width - 50) tank.x = canvas.width - 50 - tank.width;
 
-    // Update granaten
     for (let i = 0; i < grenades.length; i++) {
         grenades[i].y += grenades[i].speed;
 
-        // Botsing detectie
         if (
             grenades[i].x < tank.x + tank.width &&
             grenades[i].x + grenades[i].width > tank.x &&
@@ -103,7 +105,6 @@ function update() {
             document.location.reload();
         }
 
-        // Verwijder granaten die uit beeld gaan
         if (grenades[i].y > canvas.height) {
             grenades.splice(i, 1);
             i--;
@@ -111,38 +112,37 @@ function update() {
     }
 }
 
-// **Teken functie**
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Tracks tekenen
-    ctx.drawImage(trackLeftImg, tank.x - tracks.width + tracks.offsetX, tank.y + tracks.offsetY, tracks.width, tracks.height);
-    ctx.drawImage(trackRightImg, tank.x + tank.width - tracks.offsetX, tank.y + tracks.offsetY, tracks.width, tracks.height);
+    ctx.save();
+    ctx.translate(tank.x + tank.width / 2, tank.y + tank.height / 2);
+    ctx.rotate((tank.tiltAngle * Math.PI) / 180);
 
-    // Lowerbody tekenen
-    ctx.drawImage(lowerbodyImg, tank.x, tank.y, tank.width, tank.height);
+    ctx.drawImage(trackLeftImg, -tank.width / 2 - tracks.width + tracks.offsetX, -tank.height / 2 + tracks.offsetY, tracks.width, tracks.height);
+    ctx.drawImage(trackRightImg, tank.width / 2 - tracks.offsetX, -tank.height / 2 + tracks.offsetY, tracks.width, tracks.height);
 
-    // Upperbody tekenen (apart roteren)
+    ctx.drawImage(lowerbodyImg, -tank.width / 2, -tank.height / 2, tank.width, tank.height);
+    
+    ctx.restore();
+
     ctx.save();
     ctx.translate(tank.x + tank.width / 2, tank.y + tank.height / 2 + upperbody.offsetY);
     ctx.rotate((upperbody.angle * Math.PI) / 180);
     ctx.drawImage(gunImg, -upperbody.width / 2, -upperbody.height / 2, upperbody.width, upperbody.height);
     ctx.restore();
 
-    // Granaten tekenen
     for (let grenade of grenades) {
         ctx.drawImage(grenadeImg, grenade.x, grenade.y, grenade.width, grenade.height);
     }
 }
 
-// **Game loop**
 function gameLoop() {
     update();
     draw();
     requestAnimationFrame(gameLoop);
 }
 
-// Start game als alle afbeeldingen geladen zijn
 Promise.all([
     new Promise(resolve => { lowerbodyImg.onload = resolve; }),
     new Promise(resolve => { trackLeftImg.onload = resolve; }),
@@ -150,6 +150,6 @@ Promise.all([
     new Promise(resolve => { gunImg.onload = resolve; }),
     new Promise(resolve => { grenadeImg.onload = resolve; })
 ]).then(() => {
-    setInterval(spawnGrenade, spawnInterval); // Start granaten-generatie
+    setInterval(spawnGrenade, spawnInterval);
     gameLoop();
 });
